7907b4fd88b94ce35aa3398677deaf7aab70f5bd
Paulo Ricardo Mergulhão <prmergu7@gmail.com>
2023-08-28 00:24:49 -0300

    aulas

---
+++ aula12/exe012.js
@@ -0,0 +1,12 @@
+var agora = new Date() // puxa data, hora, mes, ano
+var hora = agora.getHours()
+console.log(`Agora são ${hora} horas`)
+if (hora < 5) {
+    console.log('Boa Madrugada')
+}else if ( hora < 12){
+    console.log('Bom Dia!')
+}else if (hora < 18){
+    console.log('Boa Tarde!')
+}else {
+    console.log('Boa Noite!')
+}
