7625d387eaf2c1b9c6722850caa8304ec7ac856b
Paulo Ricardo Mergulhão <prmergu7@gmail.com>
2023-08-31 16:38:09 -0300

    aula 14

---
+++ aula13/do_while.js
@@ -0,0 +1,7 @@
+//faca = do  estrutura de repetição no final
+
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while(c <= 6)
---
+++ aula13/while.js
@@ -0,0 +1,8 @@
+//enquanto = while estrutura de repetição no inicio
+
+var c = 1
+while(c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+
+}
